624eb2b675801eeec712592ce5e142911e819bfd
David Kim Brady <davidekimb15@gmail.com>
2020-04-18 15:10:52 -0400

    Add server start script

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose')
+const path = require('path');
 
 require('dotenv').config();
 
@@ -9,6 +10,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'build')));
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
@@ -17,6 +19,10 @@ const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 })
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
 
 const booksRouter = require('./routes/books');
 
